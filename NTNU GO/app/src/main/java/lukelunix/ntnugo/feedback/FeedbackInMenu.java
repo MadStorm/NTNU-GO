2a85fa5333087ebdd411bbe499414dfc86971de3
MadStorm <stormlunix@gmail.com>
2017-03-09 23:37:02 -0500

    Updating app name
    Updating app name from NTNU mPLE to NTNU GO

--- NTNU GO/app/src/main/java/lukelunix/ntnugo/feedback/FeedbackInMenu.java
+++ NTNU GO/app/src/main/java/lukelunix/ntnugo/feedback/FeedbackInMenu.java
@@ -1,26 +1,34 @@
 package lukelunix.ntnugo.feedback;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import static java.security.AccessController.getContext;
+
 /**
  * Created by Luka on 08.03.2016.
  */
 public class FeedbackInMenu {
 
-    private final static String APP_TITLE = "NTNU mPLE";// App Name
+    private final static String APP_TITLE = "NTNU GO";// App Name
 
 
     public void showFeedbackDialog(final Context mContext) {
         final Dialog dialog = new Dialog(mContext);
         dialog.setTitle("Feedback " + APP_TITLE);
+
+
+
 
         LinearLayout layout = new LinearLayout(mContext);
         layout.setOrientation(LinearLayout.VERTICAL);
@@ -28,9 +36,12 @@ public class FeedbackInMenu {
         TextView textView = new TextView(mContext);
         textView.setText("If you enjoy using " + APP_TITLE + ", please take a moment to rate it.\n Thanks for your support!");
         textView.setGravity(Gravity.CENTER);
-        textView.setWidth(240);
-        textView.setPadding(4, 0, 4, 10);
+        textView.setWidth(getScreenWidth());
+        textView.setHeight(getScreenHeight()/4);
+        //textView.setPadding(10,10,10,10);
         layout.addView(textView);
+
+
 
         /*
         Button b1 = new Button(mContext);
@@ -47,7 +58,7 @@ public class FeedbackInMenu {
         b2.setText("Rate " + APP_TITLE);
         b2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=lukelunix.ntnugo&hl=no")));
+                mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=lukelunix.ntnumple")));
                 // dialog.dismiss();
             }
         });
@@ -64,5 +75,13 @@ public class FeedbackInMenu {
 
         dialog.setContentView(layout);
         dialog.show();
+    }
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
 }
